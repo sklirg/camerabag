d662fca4d98970cf4611c7492297339fe092b203
Håkon Solbjørg <hlsolbjorg@gmail.com>
2019-04-05 00:19:47 +0200

    feat: Make it possible to initiate S3 sync from admin.

--- bag/apps/gallery/admin.py
+++ bag/apps/gallery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Gallery, Image
+from .management.commands.s3_gallery_sync import Command as SyncCommand
 
 
 def make_public(modeladmin, request, queryset):
@@ -11,13 +12,19 @@ def make_not_public(modeladmin, request, queryset):
     queryset.update(public=False)
 
 
+def sync_galleries_with_s3(modeladmin, request, queryset):
+    cmd = SyncCommand()
+    for gallery in queryset.all():
+        cmd.handle(gallery_id=gallery.id)
+
+
 @admin.register(Gallery)
 class GalleryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ['title', 'public', 'number_of_images']
     list_filter = ['public']
     search_fields = ['title']
-    actions = [make_public, make_not_public]
+    actions = [make_public, make_not_public, sync_galleries_with_s3]
 
     def number_of_images(self, obj):
         return obj.image_set.count()
